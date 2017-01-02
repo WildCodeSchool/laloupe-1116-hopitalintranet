597ae3604f85d102845eafbd435bda49445f291d
sylvie <retatsylvie@gmail.com>
2017-01-02 15:35:00 +0100

    page organigrammes notes journaux direction certifications

---
+++ src/HopitalBundle/Entity/Noteservice.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace HopitalBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Noteservice
+ */
+class Noteservice
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
