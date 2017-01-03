ec52a38d938c7d2166dec9e3865e52552f1ab780
boodmandiouf <l.picard974@laposte.net>
2017-01-03 16:41:41 +0100

    ajout de l'agenda

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,8 +15,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new HopitalBundle\HopitalBundle(),
+            new AppBundle\AppBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
