e70ac39323fd501aa9ccd75dd9a3bae01179cbdb
sylvie <retatsylvie@gmail.com>
2017-01-11 18:02:38 +0100

    astreintes

--- src/HopitalBundle/Entity/Journaux.php
+++ src/HopitalBundle/Entity/Journaux.php
@@ -5,10 +5,68 @@ namespace HopitalBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * Journaux
+
+Journaux
  */
 class Journaux
 {
+    public $file1;
+
+    protected function getUploadDir()
+    {
+        return 'uploads';
+
+    }
+
+    protected function getUploadRootDir()
+    {
+        return __DIR__.'/../../../web/'.$this->getUploadDir();
+
+    }
+
+    public function getWebPath()
+    {
+        return null === $this->image ? null : $this->getUploadDir().'/'.$this->image;
+
+    }
+
+    public function getAbsolutePath()
+    {
+        return null === $this->image ? null : $this->getUploadRootDir().'/'.$this->image;
+
+    }
+
+    public function preUpload1()
+    {
+        if (null !== $this->file1) {
+            // do whatever you want to generate a unique name
+            $this->journauximg = uniqid().'.'.$this->file1->guessExtension();
+        }
+    }
+
+    public function upload1()
+    {
+        if (null === $this->file1) {
+            return;
+        }
+
+        // if there is an error when moving the file, an exception will
+        // be automatically thrown by move(). This will properly prevent
+        // the entity from being persisted to the database on error
+        $this->file1->move($this->getUploadRootDir(), $this->journauximg);
+
+        unset($this->file1);
+    }
+
+    public function removeUpload1()
+    {
+        if ($file1 = $this->getAbsolutePath()) {
+            unlink($file1);
+        }
+    }
+
+
+
     /**
      * @var int
      */
@@ -18,10 +76,98 @@ class Journaux
     /**
      * Get id
      *
-     * @return integer 
+     * @return integer
      */
     public function getId()
     {
         return $this->id;
     }
+
+
+    /**
+     * @var string
+     */
+    private $image;
+
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     * @return Journaux
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+
+    /**
+     * @var string
+     */
+    private $journauximg;
+
+
+
+
+    /**
+     * Set journauximg
+     *
+     * @param string $journauximg
+     * @return Journaux
+     */
+    public function setJournauximg($journauximg)
+    {
+        $this->journauximg = $journauximg;
+
+        return $this;
+    }
+
+    /**
+     * Get journauximg
+     *
+     * @return string
+     */
+    public function getJournauximg()
+    {
+        return $this->journauximg;
+    }
+
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function preUpload()
+    {
+        // Add your code here
+    }
+
+    /**
+     * @ORM\PostPersist
+     */
+    public function upload()
+    {
+        // Add your code here
+    }
+
+    /**
+     * @ORM\PostRemove
+     */
+    public function removeUpload()
+    {
+        // Add your code here
+    }
+
 }
