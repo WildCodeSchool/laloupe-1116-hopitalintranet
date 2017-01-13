03c4d5980d2960ca2ac381bcfbe510c7c5a6a1af
VERO56 <you@example.com>
2017-01-13 14:52:39 +0100

    certification

---
+++ src/HopitalBundle/Entity/Certification.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace HopitalBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Certification
+ */
+class Certification
+{
+    public $file1;
+
+    protected function getUploadDir()
+    {
+        return 'uploads';
+    }
+
+    protected function getUploadRootDir()
+    {
+        return __DIR__ . '/../../../web/' . $this->getUploadDir();
+    }
+
+    public function getWebPath()
+    {
+        return null === $this->image ? null : $this->getUploadDir() . '/' . $this->image;
+    }
+
+    public function getAbsolutePath()
+    {
+        return null === $this->image ? null : $this->getUploadRootDir() . '/' . $this->image;
+    }
+
+
+    public function preUpload1()
+    {
+        if (null !== $this->file1) {
+            // do whatever you want to generate a unique name
+            $this->certificationimg = uniqid() . '.' . $this->file1->guessExtension();
+        }
+    }
+
+    public function upload1()
+    {
+        if (null === $this->file1) {
+            return;
+        }
+
+        // if there is an error when moving the file, an exception will
+        // be automatically thrown by move(). This will properly prevent
+        // the entity from being persisted to the database on error
+        $this->file1->move($this->getUploadRootDir(), $this->certificationimg);
+
+        unset($this->file1);
+    }
+
+    public function removeUpload1()
+    {
+        if ($file1 = $this->getAbsolutePath()) {
+            unlink($file1);
+        }
+    }
+
+
+    /**
+     * @var int
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * @var string
+     */
+    private $image;
+
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     * @return Certification
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+
+    /**
+     * @var string
+     */
+    private $certificationimg;
+
+
+    /**
+     * Set certificationimg
+     *
+     * @param string $certificationimg
+     * @return Certification
+     */
+    public function setCertificationimg($certificationimg)
+    {
+        $this->certificationimg = $certificationimg;
+
+        return $this;
+    }
+
+    /**
+     * Get certificationimg
+     *
+     * @return string
+     */
+    public function getCertificationimg()
+    {
+        return $this->certificationimg;
+    }
+
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function preUpload()
+    {
+        // Add your code here
+    }
+
+    /**
+     * @ORM\PostPersist
+     */
+    public function upload()
+    {
+        // Add your code here
+    }
+
+    /**
+     * @ORM\PostRemove
+     */
+    public function removeUpload()
+    {
+        // Add your code here
+    }
+
+
+}
