ecd0d43ad592753d663a462a433546be27c711c3
Celine Langlade <Cendrillon@MacBook-Pro-de-Celine.local>
2017-01-19 10:53:53 +0100

    lien vers mon compte du profil de Sacha

--- src/Vl/AnnonceBundle/Controller/AnnonceController.php
+++ src/Vl/AnnonceBundle/Controller/AnnonceController.php
@@ -8,6 +8,8 @@ use Vl\AnnonceBundle\Entity\Amicale;
 use Vl\AnnonceBundle\Entity\Commentaire;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use IuchBundle\Entity\Service;
+use IuchBundle\Entity\ServiceRepository;
 
 /**
  * Annonce controller.
@@ -45,9 +47,11 @@ class AnnonceController extends Controller
         $em = $this->getDoctrine()->getManager();
 
         $amicales = $em->getRepository('VlAnnonceBundle:Amicale')->findAll();
+        $services = $em->getRepository('IuchBundle:Service')->findAll();
 
         return $this->render('VlAnnonceBundle:annonce:indexAmicale.html.twig', array(
             'amicales' => $amicales,
+            'services' => $services
         ));
     }
 
